6924d1671b3d02ebd730341b3d7f5a8b34928b62
Vladimir Bukvić <vladimir.bukvic@live.com>
2015-05-07 08:57:52 +0000

    Blog and posts spec - context

--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -2,16 +2,26 @@ require "blog"
 require "post"
 
 describe Blog do
-    it "has no posts" do
-        expect(Blog.new).to be_empty
-    end
-
-    it "has a post when initialized with one" do
-        expect(Blog.new(posts: [Post.new])).not_to be_empty
+    
+    context "with no posts" do
+        let(:blog) { Blog.new }
+        
+            it "has no posts" do
+                expect(Blog.new).to be_empty
+            end
     end
     
-    it "counts the number of posts" do
-         expect(Blog.new(posts: [Post.new]).posts_count).to eq 1
-    end
-    
+    context "with one post" do # u kontekstu refaktorujemo
+        let(:blog) { Blog.new(posts: [Post.new]) } # ovo je lazy varijabla
+        # ona se svaki put inicijalizuje, znaci da ako je u jednom testu promenimo
+        # to se ne odnosi na drugi test
+        
+            it "has a post when initialized with one" do
+                expect(blog).not_to be_empty
+            end
+            
+            it "counts the number of posts" do
+                 expect(blog.posts_count).to eq 1
+            end
+    end    
 end
