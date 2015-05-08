7e40b7748f10085a28ed21d44caa8c6176a4123e
Vladimir Bukvić <vladimir.bukvic@live.com>
2015-05-08 06:57:09 +0000

    Mocks

--- spec/stub_spec.rb
+++ spec/stub_spec.rb
@@ -1,4 +1,4 @@
-class Product
+class Product2
     attr_reader :price
 end
 
@@ -15,11 +15,12 @@ class PriceCalculator
 end
 
 describe "Stubs" do
+    let(:calculator) {PriceCalculator.new}
     
     it "provides stubs to stinulate state" do
-        calculator = PriceCalculator.new
+        #calculator = PriceCalculator.new
         
-        product_stub = instance_double("Product")
+        product_stub = instance_double("Product2")
         #allow(first_product).to receive(:price).and_return(1.0,100.25)
         allow(product_stub).to receive(:price).and_return(1.0, 100.25)
         # jedino sto mora, to je da primalac bude definisan, tj `price` metod
@@ -30,8 +31,26 @@ describe "Stubs" do
         calculator.add product_stub
         #2.times {calculator.add product_stub }
         
-        #calculator.add instance_double("Product", price: 100.25)
+        #calculator.add instance_double("Product2", price: 100.25)
         
         expect(calculator.final_price).to eq 101.25
+    end
+    
+    it "provides mocks to assert on message passing" do
+        product_mock = double(:product2)
+        #expect(product_mock).to receive(:price)
+        #expect(product_mock).to receive(:price).with(2,4,5)
+        expect(product_mock).to receive(:price).with(any_args) # ili no_args
+        
+        calculator.add product_mock
+        calculator.final_price # sa ovim metodom prolazi, jer on poziva :price metod gore je definisan
+    end
+    
+    it "provides mocks to assert on message passing 2" do
+        #expect_any_instance_of(Product2).to receive(:price) { 5 } # i da vrati 5
+        allow_any_instance_of(Product2).to receive(:price) { 5 } # i da vrati 5
+        
+        product = Product2.new
+        puts product.price
     end
 end
