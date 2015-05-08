00df730c5f234c3684bd655bb1952d880ae62d7b
Vladimir Bukvić <vladimir.bukvic@live.com>
2015-05-08 06:35:57 +0000

    Stubs 2

--- spec/stub_spec.rb
+++ spec/stub_spec.rb
@@ -7,7 +7,7 @@ class PriceCalculator
         products << product
     end
     def products
-    @products ||= []
+        @products ||= []
     end
     def final_price
         @products.map(&:price).inject(&:+)
@@ -19,14 +19,19 @@ describe "Stubs" do
     it "provides stubs to stinulate state" do
         calculator = PriceCalculator.new
         
-        first_product = instance_double("Product")
-        allow(first_product).to receive(:price).and_return(1.0,100.25)
+        product_stub = instance_double("Product")
+        #allow(first_product).to receive(:price).and_return(1.0,100.25)
+        allow(product_stub).to receive(:price).and_return(1.0, 100.25)
+        # jedino sto mora, to je da primalac bude definisan, tj `price` metod
         
-        calculator.add first_product # => 1.0
-        calculator.add first_product # => 100.25
+        #calculator.add first_product # => 1.0
+        #calculator.add first_product # => 100.25
+        calculator.add product_stub
+        calculator.add product_stub
+        #2.times {calculator.add product_stub }
         
         #calculator.add instance_double("Product", price: 100.25)
         
-    expect(calculator.final_price).to eq 101.25
+        expect(calculator.final_price).to eq 101.25
     end
 end
